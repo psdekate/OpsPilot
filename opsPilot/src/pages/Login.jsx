ca3bfa4cea51e61c3989473bbf9c1684d5b327c3
Piyush Dekate <148255470+psdekate@users.noreply.github.com>
2026-04-22 23:43:19 +0530

    zod implementation
    implementing zod for form controls

--- opsPilot/src/pages/Login.jsx
+++ opsPilot/src/pages/Login.jsx
@@ -3,50 +3,71 @@ import { useNavigate } from "react-router-dom";
 import useAuthStore from "../store/AuthStore";
 import "../pages/Login.css";
 
+const loginSchema = z.object({
+  email: z.string().email("Invalid email"),
+  password: z.string().password(6, "Minimum 6 chars"),
+});
+
 export default function Login() {
   const login = useAuthStore((state) => state.login);
   const navigate = useNavigate();
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: zodResolver(loginSchema) });
 
-  function handleSubmit(e) {
-    e.preventDefault();
-
-    if (!email.includes("@")) {
-      setError("Invalid email");
-      return;
-    }
-
-    if (password.length < 6) {
-      setError("Password should be atleast 6 characters long");
-      return;
-    }
-
-    setError("");
+  function onSubmit(data) {
     login();
     navigate("/");
   }
 
+  //   const [email, setEmail] = useState("");
+  //   const [password, setPassword] = useState("");
+  //   const [error, setError] = useState("");
+
+  //   function handleSubmit(e) {
+  //     e.preventDefault();
+
+  //     if (!email.includes("@")) {
+  //       setError("Invalid email");
+  //       return;
+  //     }
+
+  //     if (password.length < 6) {
+  //       setError("Password should be atleast 6 characters long");
+  //       return;
+  //     }
+
+  //     setError("");
+  //     login();
+  //     navigate("/");
+  //   }
+
   return (
     <div className="login-page">
       <h2 style={{ textTransform: "uppercase" }}>Login to continue</h2>
-      <form action="" onSubmit={handleSubmit} className="login-form">
+      <form action="" onSubmit={handleSubmit(onSubmit)} className="login-form">
         <div className="inputs">
           <input
             type="text"
             placeholder="Enter your email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            {...register("email")}
+            // value={email}
+            // onChange={(e) => setEmail(e.target.value)}
           />
+          {errors.email && <p>{errors.email.message}</p>}
           <input
             type="password"
             placeholder="Enter your password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            {...register("password")}
+            // value={password}
+            // onChange={(e) => setPassword(e.target.value)}
           />
-          {error && <p className="error-code">{error}</p>}
+          {errors.password && (
+            <p className="error-code">{errors.password.message}</p>
+          )}
         </div>
 
         <button type="submit">Login</button>
